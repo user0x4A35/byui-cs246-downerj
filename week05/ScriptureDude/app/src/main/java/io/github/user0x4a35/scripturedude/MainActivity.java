ea1b01544ef41ecb19ee0bdf4a45aae6efa72065
downerj <downer.james.256k@gmail.com>
2018-10-17 21:39:57 -0600

    Prove05: Add stretch challenge

--- week05/ScriptureDude/app/src/main/java/io/github/user0x4a35/scripturedude/MainActivity.java
+++ week05/ScriptureDude/app/src/main/java/io/github/user0x4a35/scripturedude/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.user0x4a35.scripturedude;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public static final String PREFS = "myPrefs";
     public static final String MSG_BOOK = "io.github.user0x4a35.scripturedude.BOOK";
     public static final String MSG_CHAPTER = "io.github.user0x4a35.scripturedude.CHAPTER";
     public static final String MSG_VERSE = "io.github.user0x4a35.scripturedude.VERSE";
@@ -18,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loadScripture(View view) {
+        Context context = this.getApplicationContext();
+        SharedPreferences pref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        EditText txtBook = (EditText) findViewById(R.id.txtBook);
+        EditText txtChapter = (EditText) findViewById(R.id.txtChapter);
+        EditText txtVerse = (EditText) findViewById(R.id.txtVerse);
+
+        String book = pref.getString(getString(R.string.label_book), "");
+        String chapter = pref.getString(getString(R.string.label_chapter), "");
+        String verse = pref.getString(getString(R.string.label_verse), "");
+
+        txtBook.setText(book);
+        txtChapter.setText(chapter);
+        txtVerse.setText(verse);
     }
 
     public void viewScripture(View view) {
--- week05/ScriptureDude/app/src/main/java/io/github/user0x4a35/scripturedude/ViewScriptureActivity.java
+++ week05/ScriptureDude/app/src/main/java/io/github/user0x4a35/scripturedude/ViewScriptureActivity.java
@@ -1,14 +1,23 @@
 package io.github.user0x4a35.scripturedude;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewScriptureActivity extends AppCompatActivity {
 
     public static final String TAG = "ViewScriptureActivity";
+    public static final String PREFS = "myPrefs";
+    private String book;
+    private String chapter;
+    private String verse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,9 @@ public class ViewScriptureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_scripture);
 
         Intent intent = getIntent();
-        String book = intent.getStringExtra(MainActivity.MSG_BOOK);
-        String chapter = intent.getStringExtra(MainActivity.MSG_CHAPTER);
-        String verse = intent.getStringExtra(MainActivity.MSG_VERSE);
+        book = intent.getStringExtra(MainActivity.MSG_BOOK);
+        chapter = intent.getStringExtra(MainActivity.MSG_CHAPTER);
+        verse = intent.getStringExtra(MainActivity.MSG_VERSE);
 
         Log.d(TAG, String.format("Received intent with %s %s:%s",
             book, chapter, verse
@@ -28,5 +37,18 @@ public class ViewScriptureActivity extends AppCompatActivity {
         lblScripture.setText(String.format("%s %s:%s",
             book, chapter, verse
         ));
+    }
+
+    public void saveScripture(View view) {
+        Context context = this.getApplicationContext();
+        SharedPreferences pref = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefEditor = pref.edit();
+        prefEditor.putString(getString(R.string.label_book), book);
+        prefEditor.putString(getString(R.string.label_chapter), chapter);
+        prefEditor.putString(getString(R.string.label_verse), verse);
+        prefEditor.apply();
+
+        Toast toast = Toast.makeText(this, "Scripture Saved!", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
