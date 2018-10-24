cb122d6e5e351f2a5c83676545177c16a3f79509
downerj <downer.james.256k@gmail.com>
2018-10-24 15:05:31 -0600

    Team+Prove06: Finish

--- week06/AsyncExample/app/src/main/java/io/github/user0x4a35/asyncexample/MainActivity.java
+++ week06/AsyncExample/app/src/main/java/io/github/user0x4a35/asyncexample/MainActivity.java
@@ -1,36 +1,38 @@
 package io.github.user0x4a35.asyncexample;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String FILE_NAME = "numbers.txt";
     private static final String TAG = "MainActivity";
-    private List<Integer> numbers;
+    private ArrayAdapter<Integer> numbersAdapter;
     ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        numbers = new ArrayList<>();
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        numbersAdapter = new ArrayAdapter<Integer>(
+                getApplicationContext(),
+                R.layout.support_simple_spinner_dropdown_item
+        );
+        ListView listNumbers = findViewById(R.id.listNumbers);
+        listNumbers.setAdapter(numbersAdapter);
+        progressBar = findViewById(R.id.progressBar);
         progressBar.setMax(10);
     }
 
@@ -39,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             public void run() {
                 FileOutputStream outputStream;
-                String toastMessage = "";
+                String toastMessage;
 
                 // save the file
                 try {
@@ -81,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 FileInputStream inputStream;
                 String toastMessage = "";
-                StringBuilder buffer = new StringBuilder();;
+                StringBuilder buffer = new StringBuilder();
                 boolean loadSuccess;
 
                 // load the file
@@ -91,22 +93,38 @@ public class MainActivity extends AppCompatActivity {
                     int count = 0;
                     progressBar.setProgress(count);
                     while ((ch = inputStream.read()) >= 0) {
-                        buffer.append((char) ch);
-                        progressBar.setProgress(++count);
-                        Thread.sleep(250);
+                        if (ch != '\n') {
+                            buffer.append((char) ch);
+                        } else {
+                            // get the number
+                            String strNum = buffer.toString();
+                            int num = Integer.parseInt(strNum);
+                            buffer.delete(0, buffer.length());
+
+                            // update the UI
+                            final int c = ++count;
+                            final int n = num;
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(c);
+                                    numbersAdapter.add(n);
+                                }
+                            });
+                            Thread.sleep(250);
+                        }
                     }
                     inputStream.close();
                     toastMessage = getResources().getString(R.string.toast_loaded);
-                    loadSuccess = true;
                 } catch (FileNotFoundException fnfe) {
                     toastMessage = getResources().getString(R.string.toast_error_no_file);
-                    loadSuccess = false;
                 } catch (IOException e) {
                     toastMessage = getResources().getString(R.string.toast_error_read);
-                    loadSuccess = false;
                 } catch (InterruptedException ie) {
                     Log.d(TAG, ie.getMessage());
                     return;
+                } catch (NumberFormatException nfe) {
+                    String msg = getResources().getString(R.string.toast_error_nan);
+                    Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
                 }
 
                 // notify
@@ -116,29 +134,20 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
                     }
                 });
-
-                // return if fail
-                if (!loadSuccess) {
-                    return;
-                }
-
-                // parse, store and notify on success
-                final String[] strNums = buffer.toString().split("\\s");
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    public void run() {
-                        for (String strNum : strNums) {
-                            try {
-                                int num = Integer.parseInt(strNum);
-                                numbers.add(num);
-                                Log.d(TAG, strNum);
-                            } catch (NumberFormatException nfe) {
-                                String msg = getResources().getString(R.string.toast_error_nan);
-                                Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    }
-                });
             }
         }).start();
+    }
+
+    public void clearList(View view) {
+        final Context context = getApplicationContext();
+        String msg;
+        if (numbersAdapter.getCount() > 0) {
+            numbersAdapter.clear();
+            msg = getResources().getString(R.string.toast_cleared);
+        } else {
+            msg = getResources().getString(R.string.toast_no_items);
+        }
+        progressBar.setProgress(0);
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 }
